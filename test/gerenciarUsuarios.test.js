b14623c70be7c5f8afd2ec3aec4bcc1c66351446
Sandra <sandra.alinee.sg@gmail.com>
2025-09-04 23:08:26 -0300

    Adicionando funções de teste em gestão de Usuários

---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,17 @@
+const { retornarUsuarios, adicionarNovoUsuario } = require ('../src/gerenciarUsuarios');
+const { expect } = require('chai');
+
+describe('Testar as funções de Gestão de Usuários', function () {
+    it('Validar a adição de um novo nome de usuário na lista', function (){
+        //1. Adicionar um novo nome na lista de usuários
+        adicionarNovoUsuario('Maria');
+
+        //2. Retornar a lista de usuários na caixa lista de usuários
+        const listaDeUsuarios = retornarUsuarios();
+
+        //3. Comparar se o novo nome está no fim da lista de usuários
+        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+
+    });
+});
+

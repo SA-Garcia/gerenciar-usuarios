2ada0ff810cde5e19249b51481c044a180d98dd4
Sandra <sandra.alinee.sg@gmail.com>
2025-09-09 23:01:11 -0300

    Criação de obj e percorrer em lista

--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,13 +4,21 @@ const { expect } = require('chai');
 describe('Testar as funções de Gestão de Usuários', function () {
     it('Validar a adição de um novo nome de usuário na lista', function (){
         //1. Adicionar um novo nome na lista de usuários
-        adicionarNovoUsuario('Maria');
+        adicionarNovoUsuario ({
+        nome: 'Maria', 
+        email: 'maria@email.com'
+    });
 
         //2. Retornar a lista de usuários na caixa lista de usuários
         const listaDeUsuarios = retornarUsuarios();
 
         //3. Comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+        //expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
+        //expect(listaDeUsuarios.at(-1).email).to.equal('maria@email.com');
+        expect(listaDeUsuarios.at(-1)).to.eql({ //to.deep.equal
+            nome: 'Maria', 
+            email: 'maria@email.com'
+        });
 
     });
 });
